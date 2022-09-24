5344de1d2734081ced5d5c73160337e61997b0a6
Moises Nantes <moisesnantes@gmail.com>
2022-09-24 11:26:02 -0400

    Jogada Especial Roque

--- Chess/src/chess/ChessPiece.java
+++ Chess/src/chess/ChessPiece.java
@@ -5,7 +5,7 @@ import boardgame.Piece;
 import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
-	
+
 	private Color color;
 	private int moveCount;
 
@@ -22,22 +22,20 @@ public abstract class ChessPiece extends Piece {
 		return moveCount;
 	}
 	
-	public void increaseMoveCount() {
-		moveCount ++;
+	protected void increaseMoveCount() {
+		moveCount++;
 	}
-	public void decreaseMoveCount() {
-		moveCount --;
+
+	protected void decreaseMoveCount() {
+		moveCount--;
 	}
-	
+
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
-
+	
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
- 	}
-	
-	
-
+	}
 }
